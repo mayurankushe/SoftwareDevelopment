ce37cdb5b5a565e657318a3012ac355f600f8fce
mayurankushe <mayurankushe9559@gmail.com>
2021-11-23 15:25:11 +0530

    This program done by lead

---
+++ SoftwareDevelopment/src/GitUse/GITClass.java
@@ -0,0 +1,12 @@
+package GitUse;
+
+public class GITClass
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hi this is GIT");
+		System.out.println("Done bt lead");
+		System.out.println("hello guys");
+		System.out.println("in starting area");
+	}
+}
